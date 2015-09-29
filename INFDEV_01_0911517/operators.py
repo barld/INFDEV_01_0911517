124548593215d12c14b0b01ad1518c9f82958a71
barld <bakeds@gmail.com>
2015-09-29 10:23:10 +0200

    add extra example to to operator demo script with string length

--- INFDEV_01_0911517/operators.py
+++ INFDEV_01_0911517/operators.py
@@ -26,3 +26,12 @@ try:
     print not isNull() & doSomething()
 except Exception as e:
     print "on no an exception:", e.message
+
+
+print "--- string length ------"
+
+s = "hallo"
+
+print len(s) > 9 and s[9] == "a"
+
+print len(s) > 9 & s[9] == "a"
